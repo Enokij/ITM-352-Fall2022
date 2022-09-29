fe230377228f1d05438d1eff99b0fde4f7d32060
Enonoki <jus10.enoki@gmail.com>
2022-09-28 19:34:32 -1000

    update
    update

---
+++ Lab8/ex3.js
@@ -0,0 +1,13 @@
+require("./products_data.js");
+var num_products = 5;
+
+for (counter = 1; counter <= num_products; counter ++) {
+    if (counter > num_products/4 && counter < num_products*3/4) {
+        console.log(eval('name' + counter) + " is sold out!");
+    }
+    else{ 
+        console.log(counter + ". " + eval('name' + counter));
+    }
+}
+
+console.log("That's all we have!");
