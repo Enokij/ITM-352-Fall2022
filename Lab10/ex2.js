c412acf2cbcce8576c678f020e2a78ee2733ba3f
Enonoki <jus10.enoki@gmail.com>
2022-10-03 03:49:29 -1000

    SPP2

---
+++ Lab10/ex2.js
@@ -0,0 +1,6 @@
+var attributes  =  "<name>;<age>;<major>";
+var pieces = attributes.split(";");
+
+for (i=0; i<pieces.length; i++){
+    console.log(`i=${i} value=${pieces[i]} type=${typeof pieces[i]}`);
+}
